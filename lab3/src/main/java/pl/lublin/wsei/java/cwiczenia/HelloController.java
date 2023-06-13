7e32043dcbd445e1fa18c349f6f7184f36d455af
Milosz <milosz.gryczkaa@gmail.com>
2023-06-13 16:04:49 +0200

    commit

--- lab3/src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ lab3/src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,16 +1,14 @@
 package pl.lublin.wsei.java.cwiczenia;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class HelloController {
     public Button btnMain;
     public TextField txtLicznik;
     int clickCounter;
-    public void onBtnAction(ActionEvent actionEvent) {
+    public void onBtnAction() {
         System.out.println("Kliknięcie nr " + ++clickCounter);
         txtLicznik.setText(String.valueOf(clickCounter));
             if (clickCounter == 1)
