b37ee1e7e401045a965defd2079601fb9a1a133c
Milosz <milosz.gryczkaa@gmail.com>
2023-06-13 15:59:39 +0200

    commit

---
+++ lab3/src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -0,0 +1,27 @@
+package pl.lublin.wsei.java.cwiczenia;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    public Button btnMain;
+    public TextField txtLicznik;
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+    int clickCounter;
+    public void onBtnAction(ActionEvent actionEvent) {
+
+        System.out.println("Kliknięcie nr " + ++clickCounter);
+        txtLicznik.setText(String.valueOf(clickCounter));
+            if (clickCounter == 1)
+                btnMain.setText("I jeszcze raz!");
+    }
+}
